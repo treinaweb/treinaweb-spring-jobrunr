a33e5d13818681658c4de5d9fac9440d1586e05a
Cleyson Lima <cleysonph@gmail.com>
2025-04-07 17:57:33 -0300

    01:03 - Criando o projeto base

---
+++ src/main/java/br/com/treinaweb/springjobrunr/SpringJobrunrApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.springjobrunr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJobrunrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJobrunrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinaweb/springjobrunr/SpringJobrunrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.springjobrunr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJobrunrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
