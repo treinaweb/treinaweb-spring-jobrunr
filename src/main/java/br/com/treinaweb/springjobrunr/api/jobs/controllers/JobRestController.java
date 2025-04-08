e6082006f3085667dacc63d0759ccd509ef182c1
Cleyson Lima <cleysonph@gmail.com>
2025-04-08 00:25:02 -0300

    02:06 - Adicionando informações extras nos jobs

--- src/main/java/br/com/treinaweb/springjobrunr/api/jobs/controllers/JobRestController.java
+++ src/main/java/br/com/treinaweb/springjobrunr/api/jobs/controllers/JobRestController.java
@@ -2,9 +2,11 @@ package br.com.treinaweb.springjobrunr.api.jobs.controllers;
 
 import java.util.Map;
 
+import org.jobrunr.scheduling.JobBuilder;
 import org.jobrunr.scheduling.JobScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.treinaweb.springjobrunr.core.jobs.SampleJob;
@@ -19,9 +21,19 @@ public class JobRestController {
     private final JobScheduler jobScheduler;
 
     @GetMapping("/fire-and-forget")
-    public Map<String, String> fireAndForget() {
-        var jobId = jobScheduler.enqueue(sampleJob::simpleJob);
-        return Map.of("jobId", jobId.toString());
+    public Map<String, String> fireAndForget(@RequestParam String arg) {
+        var job = JobBuilder.aJob()
+            .withName("Sample Job - Simple Job With Arg")
+            .withLabels("sample-job", "simple-job-with-arg", "fire-and-forget")
+            .withDetails(() -> sampleJob.simpleJobWithArg(arg));
+        var job1Id = jobScheduler.create(job);
+
+        var job2Id = jobScheduler.enqueue(sampleJob::simpleJob);
+
+        return Map.of(
+            "job1Id", job1Id.toString(),
+            "job2Id", job2Id.toString()
+        );
     }
     
 }
--- src/main/java/br/com/treinaweb/springjobrunr/core/jobs/SampleJob.java
+++ src/main/java/br/com/treinaweb/springjobrunr/core/jobs/SampleJob.java
@@ -1,5 +1,6 @@
 package br.com.treinaweb.springjobrunr.core.jobs;
 
+import org.jobrunr.jobs.annotations.Job;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,13 @@ public class SampleJob {
     
     private final Logger log = LoggerFactory.getLogger(SampleJob.class);
 
+    @Job(name = "Sample Job - Simple Job", labels = { "smaple-job", "simple-job", "fire-and-forget" })
     public void simpleJob() {
-        log.info("Exexuting a simple job");
+        log.info("Executing a simple job");
+    }
+
+    public void simpleJobWithArg(String arg) {
+        log.info("Executing a simple job with arg: " + arg);
     }
 
 }
